fdd3cd0c02855dc78739248f7e80c455ff10c105
gil <gilovi@users.noreply.github.com>
2015-04-19 20:34:40 +0300

    submitted ex2.

--- ex2/Debug/main.d
+++
@@ -1,3 +0,0 @@
-main.d: ../main.cpp ../uthreads.h
-
-../uthreads.h:
--- ex2/Debug/thread.d
+++
@@ -1,7 +0,0 @@
-thread.d: ../thread.cpp ../thread.h ../general.h ../uthreads.h
-
-../thread.h:
-
-../general.h:
-
-../uthreads.h:

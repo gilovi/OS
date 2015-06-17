c7f2cd4dbd3c9ea16e268bdb70fb860cc62f513e
mosem <mandelmoshe@gmail.com>
2015-06-17 21:07:01 +0300

    working after sanity check

--- ex5/general.h
+++ ex5/general.h
@@ -8,10 +8,16 @@
 #ifndef GENERAL_H_
 #define GENERAL_H_
 
+#include <errno.h>
+#include <iostream>
+
 #define SUCCESS 0
 #define ERROR -1
 #define ERROR_CODE 1
 #define BUFF_SIZE 1024
+#define MIN_PORT_NUM 1
+#define MAX_PORT_NUM 65535
 
+#define sysErr(sysCall, err) "Error: function:" << sysCall << " errno:" << err << "."
 
 #endif /* GENERAL_H_ */
